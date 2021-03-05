fe9067f1a99efa68d9ff1070100020e3a4b3a64b
Nutthakorn043 <yonpinw084@gmail.com>
2021-03-05 21:09:38 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,6 @@
+  
 <template>
- <div id="app">
-    <div id="nav">
-     <b-navbar toggleable="lg" type="dark" variant="warning">
-    <b-navbar-brand href="#"  > <router-link to="/">Color Grop</router-link></b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
- <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="outline-dark">Search</b-button>
-        </b-nav-form>
-      </b-navbar-nav>
-  </b-navbar>
-</div>
-      
-    
+  <div id="app">
     <router-view />
   </div>
 </template>
@@ -26,6 +12,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: black;
 
 }
 #nav {
@@ -33,7 +20,7 @@
 }
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #ffffff;
 }
 #nav a.router-link-exact-active {
   color: #ffffff;
--- src/components/axios.vue
+++ src/components/axios.vue
@@ -1,79 +1,90 @@
-<template >
-<div >
-  
-    <b-card >
-         
-    <h1 class="h1" >Color...</h1>
-         
-     <b-button variant="outline-warning" @click="loadUser">แสดงข้อมูลกลุ่มสี</b-button>
-     </b-card>
-     <div align="center"> 
-    <b-row cols-lg="3" cols-md="3" cols-sm="1">
-    <b-card 
-    v-for="photos in photoList" :key="photos.id"
-    img-src=""
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 200rem;"
-    class="card " >
-    
-    <h1>Color {{photos.albumId}}</h1>
-      <b-row class="container">
-      <b-col md="8" lg="8" sm="7">
-      <b-card-text>
-       <h3 align="left"> {{photos.id}}</h3> 
-        <br> 
-       <h5 align="left">  {{photos.title}}  </h5>  
-       </b-card-text>
-       </b-col>
-       <b-col>
-       <img :src="photos.thumbnailUrl" alt="" sizes="150px" srcset="">
-       </b-col>
-      </b-row> 
-        <a :href="photos.url">ภาพขนาด 150x150</a>
-    </b-card>
-    </b-row>   
-    </div> 
+<template>
+  <div>
+      <b-navbar toggleable="lg" type="dark" variant="warning">
+        <b-navbar-brand href="#"><img src="https://defaeroreport.com/wp-content/uploads/2019/07/nasa-logo-web-rgb.png" style="max-width: 10rem;" alt=""></b-navbar-brand>
+       
+        <router-view />
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="danger" >Search</b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    <body background="https://i.redd.it/4q0mpffzcbk41.jpg">
+    <hr />
+    <div class="container">
+      <b-row>
+        />
+
+        <h1 align="center" style="color:#ffea00">Coronal Mass Ejection (CME)</h1></b-row>
       
-    </div>
+        <br /><br />
+      
+        <b-row cols-lg="5">
+            
+          <b-card
+            v-for="user in userList"
+            :key="user.results"
+            title=""
+            img-src="https://i.pinimg.com/474x/95/11/59/951159208dc167f6c0e69d1c14139d1d.jpg"
+            img-alt=""
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+            bg-variant="light"
+            colspan="12"
+            
+          >
+            ID : {{ user.activityID }}<br /><br />
+            {{ user.catalog }}<br /><br />
+            Time: {{ user.startTime }}<br /><br />
+            {{ user.note }}
+            <a :href="user.link"
+            
+              ><br /><br />
+               <hr /><b-button variant="warning">ข้อมูลเพิ่มเติม</b-button></a
+            >
+          </b-card>
+          
+        </b-row>
+     
+     </div>
+    </body>
+     </div>
   
- 
 </template>
 
 <script>
-import axios from"axios"; 
+import axios from "axios";
 export default {
-    data(){
-        return{
-        photoList:null,
-         url:"http://jsonplaceholder.typicode.com/photos"
-        };
-    },
-     methods:{
-        loadUser(){
-            axios.get(this.url)
-            .then( response=>{ this.photoList = response.data;
-            } )
-            .catch(err => { console.log(err) } )
-        },
-     }
+  data() {
+    return {
+      userList: null,
+    };
+  },
+  mounted() {
+    let url =
+      "https://api.nasa.gov/DONKI/CME?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key=DEMO_KEY";
+    axios
+      .get(url)
+      .then((response) => {
+        this.userList = response.data;
+      })
+      .catch();
+    (err) => {
+      console.log(err);
+    };
+  },
 };
 </script>
- <style>
+<style>
 
-.h1{
-    color:#f7d307;
-}
-.h2{
-   color: rgb(231, 238, 235);
-   width: 12;
-   font: 1em sans-serif;
-}
-.card{
-    color: rgb(0, 0, 0);
-}
-.blackground{
-    color: black;
-}
+
+
 </style>
